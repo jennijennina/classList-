26ac153e9ab29fb847b27cfbf41a564881ef3ed4
jennijennina <supercoder@airvonspercoder.localdomain>
2022-08-25 22:52:51 +0200

    classList

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+let bucketList = document.getElementById("bucketList");
+let wish = document.getElementById("wish");
+
+function red() {
+  bucketList.classList.add("red");
+}
+red();
+
+function aquamarine() {
+  wish.classList.add("aquamarine");
+}
+aquamarine();
